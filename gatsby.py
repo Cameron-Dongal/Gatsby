eaa3a1f73c58d011c31f117633323a759159b1e3
Cameron-Dongal <c.a.donald@icloud.com>
2024-08-10 17:55:13 -0700

    buy/sell spy based on indicators

--- gatsby.py
+++ gatsby.py
@@ -3,14 +3,10 @@ from lumibot.brokers import Alpaca
 from lumibot.strategies.strategy import Strategy
 from lumibot.backtesting import YahooDataBacktesting
 
+
 from datetime import datetime
 
-from tradingview_ta import TA_Handler, Interval, Exchange
-import tradingview_ta
-
-API_KEY = "PKB6179NROHZUW4OM9UN"
-API_SECRET = "xnAZ6rDWg8yrvdwl1V3PnkIBWGcWt0ikKWSdJCY2"
-API_URL = "https://paper-api.alpaca.markets/v2"
+from config import API_URL,API_SECRET,API_KEY
 
 ALPACA_CONFIG = {
     "API_KEY": API_KEY,
@@ -18,33 +14,49 @@ ALPACA_CONFIG = {
     "PAPER":  True
 }
 
+from tradingview_ta import TA_Handler, Interval, Exchange
+import tradingview_ta
+
 class TestStrategy(Strategy):
     parameters = {
         "symbol": "SPY",
         "quantity": 1,
         "side": "buy"
     }
-
-    def initialize(self, symbol=""):
-        self.sleeptime = "180M"
+    
+    def initialize(self, symbol="SPY"):
+        self.sleeptime = "5M"
+        self.last_trade = "sell"
+        self.minutes_before_closing = 8
 
     def on_trading_iteration(self):
+
+        output = TA_Handler(symbol='SPY',
+                    screener='america',
+                    exchange='AMEX',
+                    interval='5m')
+        
+        rec5 = output.get_analysis()["RECOMMENDATION"]
+
         symbol = self.parameters["symbol"]
         quantity = self.parameters["quantity"]
         side = self.parameters["side"]
-        order = self.create_order(symbol, quantity, side)
-        self.submit_order(order)
 
+        if "BUY" or "STRONG_BUY" in rec5 and self.last_trade == "sell":
+            order = self.create_order(symbol, quantity, side)
+            self.last_trade = "buy"
+            self.parameters = "sell"
+            self.submit_order(order)
+        elif "SELL" or "STRONG_SELL" in rec5 and self.last_trade == "buy":
+            order = self.create_order(symbol, quantity, side)
+            self.parameters["side"]="buy"
+            self.last_trade="sell"
+            self.submit_order(order)   
+            
 trader = Trader()
 broker = Alpaca(ALPACA_CONFIG)
-strategy = TestStrategy(broker=broker, parameters={"symbol": "SPY"})
+strategy = TestStrategy(broker=broker, parameters={"symbol":"SPY"})
 
-backtesting_start = datetime(2021, 1, 1)
-backtesting_end = datetime(2024, 8, 9)
-
-strategy.backtest(YahooDataBacktesting,backtesting_start,backtesting_end,parameters={"symbol": "SPY"})
-
-#trader.add_strategy(strategy)
-#trader.run_all()
-
+trader.add_strategy(strategy)
+trader.run_all()
 
