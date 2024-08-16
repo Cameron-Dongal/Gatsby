38efcd899895541923decb55e40697c5cb85fe9c
Cameron-Dongal <c.a.donald@icloud.com>
2024-08-15 17:46:27 -0700

    dghf

--- gatsby.py
+++ gatsby.py
@@ -19,38 +19,43 @@ import tradingview_ta
 
 class TestStrategy(Strategy):
     parameters = {
-        "symbol": "BTC/USD",
+        "symbol": "BTC/USD",#change to spy / btc/usd
         "quantity": .1,
         "side": "buy"
     }
 
     
-    def initialize(self, symbol="SPY"):#change to spy / btc/usd
+    def initialize(self, symbol="BTC/USD"):#change to spy / btc/usd
         self.sleeptime = "1M"
         self.last_trade = "sell"
-        self.minutes_before_closing = 8
-        #self.set_market('24/7') #delete when not using crypto to test afterhours, and replace btc symbols and exchange with spy and amex
+        #self.minutes_before_closing = 8
+        self.set_market('24/7') #delete when not using crypto to test afterhours, and replace btc symbols and exchange with spy and amex
 
     def on_trading_iteration(self):
         
-        print("last trade: ", self.last_trade)
+        print(" ")
 
-        output = TA_Handler(symbol='SPY', #change to spy / btcusdc
-                    screener='america', #change to america / crytpo
-                    exchange='amex', #change to amex / kraken
+        output = TA_Handler(symbol='btcusdc', #change to spy / btcusdc
+                    screener='crypto', #change to america / crytpo
+                    exchange='kraken', #change to amex / kraken
                     interval='1m')
      
         rec5 = output.get_analysis().summary["RECOMMENDATION"]
 
-        print(output.get_analysis().summary)
-        print(rec5)
-
         symbol = self.parameters["symbol"]
         quantity = self.parameters["quantity"]
 
+        position = self.get_position(symbol)
+
+        print(symbol, " : ", output.get_analysis().summary)
+        print(rec5, " recommended, checking position...")
+
+
+        print("Position:", position)
+
         if rec5 == "BUY" or rec5 == "STRONG_BUY":
             if self.last_trade == "sell":
-                print("Currently buying")
+                print("Entering position...")
                 order = self.create_order(symbol, quantity, "buy")
                 self.last_trade = "buy"
                 self.parameters["side"] = "sell"
@@ -58,11 +63,18 @@ class TestStrategy(Strategy):
 
         elif rec5 == "SELL" or rec5 == "STRONG_SELL":
             if self.last_trade == "buy":
-                print("Currently selling")
-                order = self.create_order(symbol, quantity, "sell")
+                print("Exiting posiition")
+                order = self.create_order(symbol, quantity, "buy")
                 self.parameters["side"] = "buy"
                 self.last_trade = "sell"
                 self.submit_order(order)
+        
+
+
+        
+        
+
+
         
     def on_abrupt_closing(self):
         self.log_message("Abrupt closing")
@@ -70,7 +82,7 @@ class TestStrategy(Strategy):
             
 trader = Trader()
 broker = Alpaca(ALPACA_CONFIG)
-strategy = TestStrategy(broker=broker, parameters={"symbol":"SPY"}) #change to spy / btc/usd
+strategy = TestStrategy(broker=broker, parameters={"symbol":"BTC/USD"}) #change to spy / btc/usd
 
 trader.add_strategy(strategy)
 trader.run_all()
