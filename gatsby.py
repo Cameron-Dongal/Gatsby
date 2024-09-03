d0506331e479d6e0c98ac2de73d12892dc400787
Cameron-Dongal <c.a.donald@icloud.com>
2024-09-03 01:20:04 -0700

    position sizing

--- gatsby.py
+++ gatsby.py
@@ -18,73 +18,83 @@ from tradingview_ta import TA_Handler, Interval, Exchange
 import tradingview_ta
 
 class TestStrategy(Strategy):
-    parameters = {
-        "symbol": "BTC/USD",#change to spy / btc/usd
-        "quantity": 1,
-        "side": "buy"
-    }
 
-    
-    def initialize(self, symbol="BTC/USD"):#change to spy / btc/usd
-        self.sleeptime = "1M"
+    def initialize(self, symbol="SPY", risk:float=.5):#change to spy / btc/usd
+        self.symbol = symbol
+        self.sleeptime = "1D"
         self.last_trade = "sell"
+        self.risk = risk
         #self.minutes_before_closing = 8
         self.set_market('24/7') #delete when not using crypto to test afterhours, and replace btc symbols and exchange with spy and amex
 
-    def on_trading_iteration(self):
-        
-        print(" ")
-
-        output = TA_Handler(symbol='btcusdc', #change to spy / btcusdc
-                    screener='crypto', #change to america / crytpo
-                    exchange='kraken', #change to amex / kraken
-                    interval='1m')
+    def technicals(self):
+        output = TA_Handler(symbol='spy', #change to spy / btcusdc
+                    screener='america', #change to america / crytpo
+                    exchange='amex', #change to amex / kraken
+                    interval='1d')
      
-        rec5 = output.get_analysis().summary["RECOMMENDATION"]
+        recommendation = output.get_analysis().summary["RECOMMENDATION"]
+        return recommendation
 
-        symbol = self.parameters["symbol"]
-        quantity = self.parameters["quantity"]
+    def sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.risk / last_price,0)
+        return cash, last_price, quantity
+    
+    def on_trading_iteration(self):
+
+        technicals = self.technicals()
+
+        cash, last_price, quantity = self.sizing()
+        symbol = self.symbol
 
 
-        print(symbol, " : ", output.get_analysis().summary)
-        print(rec5, " recommended, checking position...")
+        if cash > last_price:
 
-        if self.last_trade == "buy":
-            print("Position: ", quantity, " of ", symbol)
-        if self.last_trade == "sell":
-            print("Position: None")
-
-
-        if rec5 == "BUY" or rec5 == "STRONG_BUY":
-            if self.last_trade == "sell":
-                print("Entering position...")
-                order = self.create_order(symbol, quantity, "buy")
+            if technicals == "BUY" or technicals == "STRONG_BUY":
+                if self.last_trade == "sell":
+                    self.sell_all()
+                order = self.create_order(
+                    symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price = last_price*1.6,
+                    stop_loss_price = last_price*0.95
+                    )
                 self.last_trade = "buy"
-                self.parameters["side"] = "sell"
                 self.submit_order(order)
 
-        elif rec5 == "SELL" or rec5 == "STRONG_SELL":
-            if self.last_trade == "buy":
-                print("Exiting position")
-                self.parameters["side"] = "buy"
+            elif technicals == "SELL" or technicals == "STRONG_SELL":
+                if self.last_trade == "buy":
+                    self.sell_all()
+                order = self.create_order(
+                    symbol,
+                    quantity,
+                    "sell",
+                    type="bracket",
+                    take_profit_price = last_price*.4,
+                    stop_loss_price = last_price *1.05
+                )
                 self.last_trade = "sell"
-                self.sell_all()#temp workaround incase buy gets filled at .9999 instead of 1 then tries to sell 1 and cant
-        
-
-
-        
-        
-
-
-        
+                self.submit_order(order)
+         
     def on_abrupt_closing(self):
         self.log_message("Abrupt closing")
         self.sell_all()
             
-trader = Trader()
-broker = Alpaca(ALPACA_CONFIG)
-strategy = TestStrategy(broker=broker, parameters={"symbol":"BTC/USD"}) #change to spy / btc/usd
+start_date = datetime(2022,1,1)
+end_date = datetime(2024, 8, 21)
 
-trader.add_strategy(strategy)
-trader.run_all()
+broker = Alpaca(ALPACA_CONFIG)
+
+strategy = TestStrategy(name='teststrat', broker=broker, parameters={"symbol":"SPY", "cash_at_risk":.5})     
+strategy.backtest(YahooDataBacktesting, start_date,end_date,parameters={"symbol":"SPY","cash_at_risk":.5})  
+
+#uncomment following to run live
+
+#trader = Trader()
+#trader.add_strategy(strategy)
+#trader.run_all()
 
