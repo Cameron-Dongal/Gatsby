24c127332dbb769fa16a200df62e286fa893f994
Cameron-Dongal <c.a.donald@icloud.com>
2024-09-06 02:44:32 -0700

    sometimes wont close long position despite negative indicators

--- gatsby.py
+++ gatsby.py
@@ -3,8 +3,12 @@ from lumibot.brokers import Alpaca
 from lumibot.strategies.strategy import Strategy
 from lumibot.backtesting import YahooDataBacktesting
 
+import requests
+
+from finbert_utils import estimate_sentiment
 
 from datetime import datetime
+from timedelta import Timedelta
 
 from config import API_URL,API_SECRET,API_KEY
 
@@ -14,26 +18,38 @@ ALPACA_CONFIG = {
     "PAPER":  True
 }
 
+HEADERS = {
+    "accept": "application/json",
+    "APCA-API-KEY-ID": API_KEY,
+    "APCA-API-SECRET-KEY": API_SECRET
+}
+
+NEWS_URL = "https://data.alpaca.markets/v1beta1/news?sort=desc"
+
 from tradingview_ta import TA_Handler, Interval, Exchange
 import tradingview_ta
+
+from macd import macd
 
 class TestStrategy(Strategy):
 
     def initialize(self, symbol="SPY", risk:float=.5):#change to spy / btc/usd
         self.symbol = symbol
         self.sleeptime = "1D"
-        self.last_trade = "sell"
+        self.current_position = None
         self.risk = risk
         #self.minutes_before_closing = 8
         self.set_market('24/7') #delete when not using crypto to test afterhours, and replace btc symbols and exchange with spy and amex
 
     def technicals(self):
-        output = TA_Handler(symbol='spy', #change to spy / btcusdc
-                    screener='america', #change to america / crytpo
-                    exchange='amex', #change to amex / kraken
-                    interval='1d')
-     
-        recommendation = output.get_analysis().summary["RECOMMENDATION"]
+#        output = TA_Handler(symbol='spy', #change to spy / btcusdc
+#                    screener='america', #change to america / crytpo
+#                    exchange='amex', #change to amex / kraken
+#                    interval='1d')
+#     
+#        recommendation = output.get_analysis().summary["RECOMMENDATION"]
+        today, seven_days_ago = self.news_start_date()
+        recommendation = macd(today)
         return recommendation
 
     def sizing(self):
@@ -42,18 +58,92 @@ class TestStrategy(Strategy):
         quantity = round(cash * self.risk / last_price,0)
         return cash, last_price, quantity
     
+    def news_start_date(self):
+        today = self.get_datetime()
+        seven_days_ago = today - Timedelta(days=7)
+        return today.strftime('%Y-%m-%d'), seven_days_ago.strftime('%Y-%m-%d')
+    
+    def fundamentals(self):
+        today,seven_days_ago = self.news_start_date()
+        PARAMETERS = {
+            "start":seven_days_ago,
+            "end":today,
+            "symbols":"SPY,QQQ,NVDA,MSFT,V,MA,XOM,MS,JPM,BLK,TLT,VIX",
+            "limit":10,
+            }
+        news = requests.get(NEWS_URL,headers=HEADERS,params=PARAMETERS)
+        news_data = news.json()
+        headlines = [event["headline"]for event in news_data["news"]]
+        probability,sentiment=estimate_sentiment(headlines)
+        return headlines,probability,sentiment
+
     def on_trading_iteration(self):
 
         technicals = self.technicals()
-
+        headlines,probability, sentiment = self.fundamentals()
         cash, last_price, quantity = self.sizing()
         symbol = self.symbol
 
+        print(headlines)
+        print(probability)
+        print(sentiment)
+
+        print(technicals)
+        print(cash)
+
+        print("\n", self.current_position)
 
         if cash > last_price:
 
-            if technicals == "BUY" or technicals == "STRONG_BUY":
-                if self.last_trade == "sell":
+            if technicals == "HOLD_LONG":
+                if self.current_position == "short":
+                    print("Closing short position")
+                    self.current_position = None
+                    self.sell_all()
+                if sentiment == "positive" and probability > .8:
+                    order = self.create_order(
+                        symbol,
+                        quantity,
+                        "buy",
+                        type="bracket",
+                        take_profit_price = last_price*1.6,
+                        stop_loss_price = last_price*0.95
+                        )
+                    self.current_position = "long"
+                    self.submit_order(order)
+                    print("Long order submitted for ", quantity, " shares")
+                if sentiment == "neutral":#to account for long term upside bias of market
+                    order = self.create_order(
+                        symbol,
+                        quantity/2,
+                        "buy",
+                        type="bracket",
+                        take_profit_price = last_price*1.6,
+                        stop_loss_price = last_price*0.95
+                        )
+                    self.current_position = "long"
+                    self.submit_order(order)
+                    print("Long order submitted for ", quantity/2, " shares")
+            elif technicals == "HOLD_SHORT":
+                if self.current_position == "long":
+                    print("Closing long position")
+                    self.sell_all()
+                    self.current_position = None
+                if sentiment == "negative" and probability > .66:
+                    order = self.create_order(
+                        symbol,
+                        quantity,
+                        "sell",
+                        type="bracket",
+                        take_profit_price = last_price*.8,
+                        stop_loss_price = last_price *1.05
+                    )
+                    self.current_position = "short"
+                    self.submit_order(order)
+                    print("Short order submitted for ", quantity, " shares")
+            elif technicals == "BUY":
+                if self.current_position == "short":
+                    print("Closing short position")
                     self.sell_all()
                 order = self.create_order(
                     symbol,
@@ -63,29 +153,36 @@ class TestStrategy(Strategy):
                     take_profit_price = last_price*1.6,
                     stop_loss_price = last_price*0.95
                     )
-                self.last_trade = "buy"
+                self.current_position = "long"
                 self.submit_order(order)
-
-            elif technicals == "SELL" or technicals == "STRONG_SELL":
-                if self.last_trade == "buy":
+                print("Long order submitted for ", quantity, " shares")
+            elif technicals == "SELL":
+                if self.current_position == "long":
+                    print("Closing long position")
+                    self.current_position = None
                     self.sell_all()
                 order = self.create_order(
                     symbol,
                     quantity,
                     "sell",
                     type="bracket",
-                    take_profit_price = last_price*.4,
-                    stop_loss_price = last_price *1.05
+                    take_profit_price = last_price*.9,
+                    stop_loss_price = last_price *1.03
                 )
-                self.last_trade = "sell"
+                self.current_position = "short"
                 self.submit_order(order)
+                print("Short order submitted for ", quantity/2, " shares")
+
          
     def on_abrupt_closing(self):
         self.log_message("Abrupt closing")
         self.sell_all()
+        self.current_position = None
             
 start_date = datetime(2022,1,1)
-end_date = datetime(2024, 8, 21)
+end_date = datetime(2022,9,15)
+
+
 
 broker = Alpaca(ALPACA_CONFIG)
 
